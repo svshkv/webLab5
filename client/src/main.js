6a5ff058f1e5eb8e3995f5c306765cc26063fa5a
Саша Руцман <rutsman@bk.ru>
2019-04-27 10:41:13 +0300

    Закончил лабу

--- client/src/main.js
+++ client/src/main.js
@@ -16,9 +16,11 @@
 import Vue from 'vue'
 import router from './router'
 import marked from 'marked'
+import axios from 'axios'
 
 let Preview = {
   props: ['text'],
+
   template: `
         <div class="editor__preview" v-html="markdownText">{{ text }}</div>
     `,
@@ -35,20 +37,74 @@ let Editor = {
   },
   data () {
     return {
+      posts: '',
       text: ''
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:8081/posts').then((response) => {
+      console.log(response.data)
+      // for (var i = 0; i < response.data.length; i++) {
+      this.posts = response.data
+      // }
+    })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  methods: {
+    addPost: function (event) {
+      // `this` внутри методов указывает на экземпляр Vue
+      var now = new Date()
+      let post = {text: this.text, date: now}
+      // user = JSON.stringify(user)
+      axios.post('http://localhost:8081/newpost', post)
+        .then((response) => {
+          alert(response)
+        })
+        .catch((error) => {
+          alert(error.response.data)
+        })
+      // `event` — нативное событие DOM
+    },
+    changePost: function (post) {
+      this.text = post.text
+      let idForDelete = {id: post._id}
+      axios.post('http://localhost:8081/deletepost', idForDelete)
+        .then((response) => {
+          alert(response)
+        })
+        .catch((error) => {
+          alert(error.response.data)
+        })
+    },
+    deletePost: function (post) {
+      let idForDelete = {id: post._id}
+      axios.post('http://localhost:8081/deletepost', idForDelete)
+        .then((response) => {
+          alert(response)
+        })
+        .catch((error) => {
+          alert(error.response.data)
+        })
     }
   },
   template: `
         <div class="editor">
             <textarea v-model="text" cols="30" rows="10" class="editor__input"></textarea>
             <preview :text="text"></preview>
+            <button v-on:click="addPost">Сохранить</button>
+
+            <h3 v-for="post in posts">
+              <p>{{ post.text }}</p><button v-on:click="deletePost(post)">Удалить</button><button v-on:click="changePost(post)">Изменить</button>
+            </h3>
         </div>
     `
 }
 
 let app = new Vue({
   el: '#app1',
-  router,
+  router: router,
   components: {
     'editor': Editor
   }
--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,17 @@ const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 const config = require('./config/config');
+const mongodb = require("mongodb")
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/posts', (req, res) => {
+app.get('/posts132123', (req, res) => {
   res.send(
     [{
       title: "Hello World!",
@@ -18,6 +23,77 @@ app.get('/posts', (req, res) => {
   )
 });
 
-app.listen(8081, function () {
-	console.log(`Server start on port ${config.port} ...`);
+
+
+// app.listen(8081, function () {
+// 	console.log(`Server start on port ${config.port} ...`);
+// });
+
+let dbClient;
+ 
+app.use(express.static(__dirname + "/public"));
+ 
+mongoClient.connect(function(err, client){
+    if(err) return console.log(err);
+    dbClient = client;
+    const db = client.db("postsdb");
+    const collection = db.collection("posts");
+    // let user = {name: "Tom", age: 23};
+    // collection.insertOne(user, function(err, result) {
+    // 	if(err){ 
+    //         return console.log(err);
+    //     }
+    app.locals.collection = client.db("postsdb").collection("posts");
+    	app.listen(8081, function(){
+        console.log("Сервер ожидает подключения...");
+
+    	});
+    //});
+});
+
+app.get("/posts", function(req, res){
+        
+    const collection = req.app.locals.collection;
+    collection.find({}).toArray(function(err, users){
+         
+        if(err) return console.log(err);
+        res.send(users)
+    });
+     
+});
+
+app.get('/newpost', function(req, res) {
+    console.log(req.body); // the posted data
+    console.log(res.body);
+
+});
+
+app.post('/newpost', function(req, res) {
+    console.log(req.body); // the posted data
+    var text = req.body.text
+    var date = req.body.date
+    const collection = req.app.locals.collection;
+    collection.insertOne(req.body, function(err, result) {
+	    if(err){ 
+	        return console.log(err);
+	    }
+	});
+});
+
+app.get('/deletepost', function(req, res) {
+    console.log(req.body); // the posted data
+    console.log(res.body);
+
+});
+
+app.post('/deletepost', function(req, res) {
+    console.log(req.body.id); // the posted data
+    const collection = req.app.locals.collection;
+    collection.remove({_id: new mongodb.ObjectID( req.body.id )}, function(err, result) {
+	    if(err){ 
+	        return console.log(err);
+	    } else {
+	    	return console.log(result);
+	    }
+	});
 });
